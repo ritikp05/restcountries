9f36d784b71c40c6e74e0001b0589ec90ae3d75e
Ritik pandey <kushagarakp10@gmail.com>
2023-11-04 20:18:32 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import Country from './Components/Country'
 import Home from './Components/Home';
 
@@ -9,12 +9,15 @@ import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <div>
+    <BrowserRouter>
+
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/country/:id' element={<Country />} />
       </Routes>
     <ToastContainer/>
+    </BrowserRouter>
     </div>
   )
 }
