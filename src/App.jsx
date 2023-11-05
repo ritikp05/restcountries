03a22cf93b1f28522badf93f93ba2e2875861337
Ritik pandey <115953701+ritikp05@users.noreply.github.com>
2023-11-05 14:48:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Navbar from './Components/Navbar'
 import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import Country from './Components/Country'
 import Home from './Components/Home';
